0daa84ed534cebb292eddf53cdd9cc070070662a
xu.pengfei <1508075252@qq.com>
2025-04-18 18:34:57 +0800

    demo-test mybatis page helper

--- src/test/java/com/faber/api/demo/student/MysqlInsertTest.java
+++ src/test/java/com/faber/api/demo/student/MysqlInsertTest.java
@@ -1,14 +1,19 @@
 package com.faber.api.demo.student;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.faber.api.base.demo.entity.Student;
 import com.faber.api.base.demo.mapper.StudentMapper;
 import com.faber.api.demo.FaTestApp;
+import com.github.pagehelper.PageHelper;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -26,6 +31,19 @@ public class MysqlInsertTest {
         student.setCorpId(1);
         student.setTenantId(1);
         studentMapper.insert(student);
+    }
+
+    @Test
+    public void testInsertList() {
+        for (int i = 0; i < 20; i++) {
+            Student student = new Student();
+            student.setId(i + 1);
+            student.setName("张三");
+            student.setAge(18);
+            student.setCorpId(1);
+            student.setTenantId(1);
+            studentMapper.insert(student);
+        }
     }
 
     @Test
@@ -47,6 +65,22 @@ public class MysqlInsertTest {
     @Test
     public void testDeleteAll() {
         studentMapper.deleteAll();
+    }
+
+    @Test
+    public void testSelect() {
+        PageHelper.startPage(3, 5);
+        List<Student> list = studentMapper.selectList(new QueryWrapper<>());
+        for (Student student : list) {
+            System.out.println("student:" + student);
+        }
+
+//        Page<Student> page = new Page<>(1, 20);
+//        QueryWrapper<Student> wrapper = new QueryWrapper<>();
+//        wrapper.eq("id", 1);
+//        Page<Student> result = studentMapper.selectPage(page, wrapper);
+//        System.out.println(result.getTotal());
+//        System.out.println(result.getRecords());
     }
 
 }
